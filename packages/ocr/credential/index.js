082f093386dd38ebb3209c7192905c79c18f662e
Satya Rohith <me@satyarohith.com>
2020-07-24 23:12:55 +0530

    set storage cors

--- packages/ocr/credential/index.js
+++ packages/ocr/credential/index.js
@@ -1,49 +1,70 @@
 async function getSignedUrl(filename) {
-    const nimbella = require('nim')
-    const bucket   = await nimbella.storage()
+  const nimbella = require('nim');
+  const bucket = await nimbella.storage();
 
-    const file = bucket.file(filename)
-    const expiration = 15 * 60 * 1000 // 15 minutes
+  await bucket.setMetadata({
+    cors: [
+      {
+        origin: ['*'],
+        responseHeader: [
+          'Content-Type',
+          'Cache-Control',
+          'Expires',
+          'Last-Modified',
+          'Content-Disposition',
+          'Access-Control-Allow-Origin',
+          'Cross-Domain'
+        ],
+        method: ['*'],
+        maxAgeSeconds: 3600
+      }
+    ]
+  });
 
-    const putOptions = {
-        version: 'v4',
-        action: 'write',
-        contentType: 'multipart/formdata; charset=UTF-8',
-        expires: Date.now() + expiration
-    }
+  const file = bucket.file(filename);
+  const expiration = 15 * 60 * 1000; // 15 minutes
 
-    const getOptions = {
-        version: 'v4',
-        action: 'read',
-        expires: Date.now() + expiration
-    }
+  const putOptions = {
+    version: 'v4',
+    action: 'write',
+    contentType: 'multipart/formdata; charset=UTF-8',
+    expires: Date.now() + expiration
+  };
 
-    return Promise
-    .all([ file.getSignedUrl(putOptions), file.getSignedUrl(getOptions) ])
+  const getOptions = {
+    version: 'v4',
+    action: 'read',
+    expires: Date.now() + expiration
+  };
+
+  return Promise.all([
+    file.getSignedUrl(putOptions),
+    file.getSignedUrl(getOptions)
+  ])
     .then(([[signedPutUrl], [signedGetUrl]]) => {
-        return {
-            body: { signedPutUrl, signedGetUrl, bucketName: bucket.id }
-        }
+      return {
+        body: {signedPutUrl, signedGetUrl, bucketName: bucket.id}
+      };
     })
     .catch(error => {
-        console.log(error)
-        return errorResponse(error.message)
-    })
+      console.log(error);
+      return errorResponse(error.message);
+    });
 }
 
-const main = (args) => {
-    if (args.filename) {
-        return getSignedUrl(args.filename)
-    } else return errorResponse('filename required')
-}
+const main = args => {
+  if (args.filename) {
+    return getSignedUrl(args.filename);
+  } else return errorResponse('filename required');
+};
 
 function errorResponse(msg) {
-    return {
-        statusCode: 400,
-        body: {
-            error: msg
-        }
+  return {
+    statusCode: 400,
+    body: {
+      error: msg
     }
+  };
 }
 
-exports.main = main
+exports.main = main;
